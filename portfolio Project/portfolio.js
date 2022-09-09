8cd6d1c0330276224d5be5e41d4b434bbe34274e
kayMmari <104101300+kayzo1903@users.noreply.github.com>
2022-09-09 15:50:45 +0300

    Add files via upload

--- portfolio Project/portfolio.js
+++ portfolio Project/portfolio.js
@@ -1,9 +1,32 @@
 //humberger menu 
 const humberger = document.querySelector(".humberger")
 const aside =document.querySelector(".aside")
+const links = document.querySelectorAll(".links")
 
 humberger.addEventListener("click",()=>{
     //console.log("hey")
     aside.classList.toggle("active")
     humberger.classList.toggle("active")
 })
+links.forEach((link)=>{
+    link.addEventListener("click", ()=>{
+        aside.classList.toggle("active")
+        humberger.classList.toggle("active") ;
+    })
+})
+
+window.addEventListener("scroll", ()=>{
+    aside.classList.remove("active")
+    humberger.classList.remove("active")
+})
+
+//date function 
+  const yearUpdate = document.getElementById("date")
+
+
+  //DOMCONTENTLOADED 
+  window.addEventListener ("DOMContentLoaded", ()=>{
+      let d = new Date()
+      yearUpdate.innerHTML = d.getFullYear()
+        
+  })
